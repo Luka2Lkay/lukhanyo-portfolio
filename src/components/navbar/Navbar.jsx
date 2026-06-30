a873860f8f81ef4fdf942594461000aa5077a34d
Luka <luka.matshebelele@gmail.com>
2026-06-30 12:30:44 +0200

    interactive menu burger

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -60,6 +60,36 @@ function Navbar() {
           {isOpen ? <X size={30} /> : <Menu size={30} />}
         </button>
       </nav>
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%" }}
+            transition={{ duration: 0.3 }}
+            className="md:hidden bg-slate-900 h-screen "
+          >
+            <ul className="flex flex-col items-center justify-center h-full gap-8 text-xl">
+              {navItems.map((item, index) => (
+                <li key={index}>
+                  <a
+                    href={item.href}
+                    onClick={() => setIsOpen(false)}
+                    className="text-white hover:text-cyan-400 transition"
+                  >
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+
+              <button className="text-white hover:text-yellow-400">
+                <Sun size={20} />
+              </button>
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 }
