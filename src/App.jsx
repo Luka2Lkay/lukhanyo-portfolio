533b5c68bf45148f41ac25a906364fff9c9ecd40
Luka <luka.matshebelele@gmail.com>
2026-07-11 04:20:15 +0200

    Projects component with boilerplate code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "@/components/navbar/Navbar";
 import Hero from "@/components/hero_section/hero/Hero";
 import About from "@/components/about_section/about/About";
+import Projects from "./components/projects_section/projects/Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
     </>
   );
 }
